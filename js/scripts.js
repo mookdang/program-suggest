09c763a43825f2d282d5a3c10f2397d18c41c0e2
mook <mookdang24@gmail.com>
2020-06-05 15:43:06 -0700

    finally make the fist if statement to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
 $(document).ready(function() {
   $("form#questions").submit(function(event) {
     const q1 = $("select#q1").val();
+    const q2 = $("select#q2").val();
 
-    if (q1 === 'a1' && q1 !== 'a2' && q1 !== 'a3') {
-      let answer = "Ruby";
-   } else if (q1 !== 'a1' && q1 === 'a2' && q1 !== 'a3') {
-      answer = "C";
-    
+      if (q1 === 'a1' && q1 !== 'a2' && q1 !== 'a3') {
+        answer = "Ruby";
+      } else if (q1 !== 'a1' && q1 === 'a2' && q1 !== 'a3') {
+        answer = "C";  
+      } else if (q1 !== 'a1' && q1 !== 'a2' && q1 === 'a3') {
+        answer = "Java";
+      } 
 
+      if (q2 === 'a1' && q2 !== 'a2' && q2 !== 'a3') {
+        answer = "Ruby";
+      } else if (q2 !== 'a1' && q2 === 'a2' && q2 !== 'a3') {
+        answer = "C";  
+      } else if (q2 !== 'a1' && q2 !== 'a2' && q2 === 'a3') {
+        answer = "Java";
+      } 
 
       $("#langauage").text(answer);
       $("#answer").show();
-    }
+
     event.preventDefault();
   });
 });

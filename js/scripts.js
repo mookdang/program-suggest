e406c1d2a433b11eb2bda3a5d2d27b84c6f15666
mook <mookdang24@gmail.com>
2020-06-05 15:26:52 -0700

    working on scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,17 @@
 $(document).ready(function() {
   $("form#questions").submit(function(event) {
     const q1 = $("select#q1").val();
-    const q2 = $("select#q2").val();
-    const q3 = $("select#q3").val();
-    const q4 = $("select#q4").val();
-    const q5 = $("select#q5").val();
 
-    const answer = "questions";
-    if (q1 === 'a1' && q2 === 'a1') {
-      answer = "Ruby";
+    if (q1 === 'a1' && q1 !== 'a2' && q1 !== 'a3') {
+      let answer = "Ruby";
+   } else if (q1 !== 'a1' && q1 === 'a2' && q1 !== 'a3') {
+      answer = "C";
+    
+
+
+      $("#langauage").text(answer);
+      $("#answer").show();
     }
-
-    $("#langauage").text(answer);
-    $("#answer").show();
-
     event.preventDefault();
   });
 });

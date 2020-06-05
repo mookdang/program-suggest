3943819f50ad01b0f1d84eb6aaebea47ff5b5f00
mook <mookdang24@gmail.com>
2020-06-05 16:18:18 -0700

    fixed script file

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,14 @@
 $(document).ready(function() {
   $("form#questions").submit(function(event) {
     const q1 = $("select#q1").val();
-    const q2 = $("select#q2").val();
 
       if (q1 === 'a1' && q1 !== 'a2' && q1 !== 'a3') {
-        answer = "Ruby";
-      } else if (q1 !== 'a1' && q1 === 'a2' && q1 !== 'a3') {
-        answer = "C";  
+        answer = "Ruby"
+      } else if (q1 !== 'a1' && q1 === 'a2' && q1 || 'a3') {
+        answer = "Javascript";  
       } else if (q1 !== 'a1' && q1 !== 'a2' && q1 === 'a3') {
-        answer = "Java";
-      } 
-
-      if (q2 === 'a1' && q2 !== 'a2' && q2 !== 'a3') {
-        answer = "Ruby";
-      } else if (q2 !== 'a1' && q2 === 'a2' && q2 !== 'a3') {
-        answer = "C";  
-      } else if (q2 !== 'a1' && q2 !== 'a2' && q2 === 'a3') {
-        answer = "Java";
-      } 
+        answer = "C";
+      }
 
       $("#langauage").text(answer);
       $("#answer").show();
